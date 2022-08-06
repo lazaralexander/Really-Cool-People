7a2283a8a72daf310ca4eaa7e85fe034788222c5
cassidytrbl <97814148+cassidytrbl@users.noreply.github.com>
2022-08-06 13:34:47 -0700

    adding error messages

--- develop/client/src/pages/HomePage.js
+++ develop/client/src/pages/HomePage.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React from "react";
 
 const HomePage = () => {
-    console.log("HomePage connected");
+  console.log("HomePage connected");
 
-    return (
-        <>
-            <h1>HomePage</h1>
-        </>
-    )
-}
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  const renderForm = (
+    <div className="form">
+      <form>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+
+  return (
+    <>
+      <div className="app">
+        <div className="login-form">
+          <div className="title">Sign In</div>
+          {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+        </div>
+      </div>
+      );
+    </>
+  );
+};
 
 export default HomePage;
